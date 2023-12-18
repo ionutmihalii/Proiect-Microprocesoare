d91cd57c90b61d3034e3da2941fb7ad2704d787b
Vlad Popescu <vlad.popescu2@mta.ro>
2023-12-18 18:49:04 +0200

    Configured PIT

--- KEIL/Pit.c
+++ KEIL/Pit.c
@@ -1,26 +1,39 @@
 #include "Pit.h"
-#include "uart.h"
 #include "gpio.h"
 
-static uint8_t change = 0;
+volatile uint8_t flag = 0;
+
 
 void PIT_Init(void) {
 	
-	// Activarea semnalului de ceas pentru perifericul PIT
-	SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;
-	// Utilizarea semnalului de ceas pentru tabloul de timere
-	PIT_MCR &= ~PIT_MCR_MDIS_MASK;
-	// Oprirea decrementarii valorilor numaratoarelor in modul debug
-	PIT->MCR |= PIT_MCR_FRZ_MASK;
-	// Setarea valoarea numaratorului de pe canalul 0 la o perioada de 1 secunda
-	PIT->CHANNEL[0].LDVAL = 0x9FFFFF;
+	/*Activarea semnalului de ceas pentru perifericul PIT */
 	
-  // Activarea întreruperilor pe canalul 0
+	SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;
+	
+	/*Utilizarea semnalului de ceas pentru tabloul de timere */
+	
+	PIT_MCR &= ~PIT_MCR_MDIS_MASK;
+	
+	/* Oprirea decrementarii valorilor numaratoarelor in modul debug */
+	
+	PIT->MCR |= PIT_MCR_FRZ_MASK;
+	
+	/* Setarea valoarea numaratorului de pe canalul 0 la o perioada de 1.476 secunde */
+	
+	PIT->CHANNEL[0].LDVAL = 0xEC28F4;
+
+	
+  /*Activarea intreruperilor pe canalul 0 */
+	
 	PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TIE_MASK;
-	// Activarea timerului de pe canalul 0
+	
+	/* Activarea timerului de pe canalul 0 */
+	
 	PIT->CHANNEL[0].TCTRL |= PIT_TCTRL_TEN_MASK;
 	
-	// Activarea întreruperii mascabile si setarea prioritatiis
+	
+	/* Activarea intreruperii mascabile si setarea prioritatii */
+	
 	NVIC_ClearPendingIRQ(PIT_IRQn);
 	NVIC_SetPriority(PIT_IRQn,5);
 	NVIC_EnableIRQ(PIT_IRQn);
@@ -28,32 +41,29 @@ void PIT_Init(void) {
 
 void PIT_IRQHandler(void) {
 	
-	if(change == 0)
+	if(PIT->CHANNEL[0].TFLG & PIT_TFLG_TIF_MASK)
 	{
-		ChangeColor(0, 0, 1);
-		change = 1;
-	}
-	
-	if(change == 1)
-	{
-		ChangeColor(1, 0, 1);
-		change = 2;
-	}
-	
-	if(change == 2)
-	{
-		ChangeColor(0, 1, 1);
-		change = 3;
-	}
-	
-	if(change == 3)
-	{
-		ChangeColor(0, 0, 0);
-		change = 0;
-	}
-	
-	if(PIT->CHANNEL[0].TFLG & PIT_TFLG_TIF_MASK) {
-		ADC0->SC1[0] |= ADC_SC1_AIEN_MASK;
 		PIT->CHANNEL[0].TFLG &= PIT_TFLG_TIF_MASK;
+		if(flag == 0) /* roz */
+		{
+			GPIOB_PCOR |= (1 << 18);	
+		}
+		if(flag == 1) /* cian */
+		{
+			GPIOB_PSOR |= (1 << 18); 
+			GPIOB_PCOR |= (1<<19);
+		}
+		if(flag == 2) /* stins */
+		{
+			GPIOD_PSOR |= (1 << 1);
+			GPIOB_PSOR |= (1<<19);
+		}
+		if(flag == 3) /* albastru */
+		{
+			GPIOD_PCOR |= (1 << 1);
+			flag = (uint8_t)-1;
+		}
+		flag++;
 	}
 }
+
--- KEIL/Pit.h
+++ KEIL/Pit.h
@@ -2,3 +2,6 @@
 
 void PIT_Init(void); // Functie de initializarea a modulului periferic
 void PIT_IRQHandler(void); // Functia de tratarea a intreruperilor pentru perifericul PIT
+uint8_t UTILS_PrintTimer(uint32_t value); // Functie menita sa printeze o valoare numarata in formatul MM:SS
+
+extern volatile uint8_t flag;
